a29ab1063cf45eff93c119279ca9baa78511cd12
roeldingle <roeldingle@gmail.com>
2017-03-07 21:30:56 +0700

    modified pages

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -17,34 +17,43 @@ $$(document).on('deviceready', function() {
 });
 
 
+//slider
+    
 
 
 // Now we need to run the code that will be executed only for About page.
 
 // Option 1. Using page callback for page (for "about" page in this case) (recommended way):
-myApp.onPageInit('about', function (page) {
+
+myApp.onPageInit('intro', function (page) {
     // Do something here for "about" page
+    //  var mySwiper = myApp.swiper('.swiper-container', {
+    //   pagination: '.swiper-pagination',
+    //   paginationHide: false,
+    //   paginationClickable: true,
+    //   nextButton: '.swiper-button-next',
+    //   prevButton: '.swiper-button-prev',
+    // });
 
 })
+
 
 // Option 2. Using one 'pageInit' event handler for all pages:
 $$(document).on('pageInit', function (e) {
     // Get page data from event data
     var page = e.detail.page;
 
-    
-    //slider
     var mySwiper = myApp.swiper('.swiper-container', {
-      pagination: '.swiper-pagination',
-      paginationHide: false,
-      paginationClickable: true,
-      nextButton: '.swiper-button-next',
-      prevButton: '.swiper-button-prev',
-    });
+        pagination: '.swiper-pagination',
+        paginationHide: false,
+        paginationClickable: true,
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+      });
+    
 
-    if (page.name === 'about') {
-        // Following code will be executed for page with data-page attribute equal to "about"
-        //myApp.alert('Here comes About page');
+    if (page.name !== 'index' || page.name !== 'about') {
+         
     }
 })
 
